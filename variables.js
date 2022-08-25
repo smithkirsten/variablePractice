8dad9a845c4885e01dd8aa4cf1214d09bf9d2088
Kirsten Smith <brownsmith.kirsten@gmail.com>
2022-08-25 12:40:38 -0400

    Add sentences and reassign variables

--- variables.js
+++ variables.js
@@ -7,3 +7,12 @@ var children = 1;
 
 var student = yes;
 var graduated = no;
+
+var sentence = `${name} is {age} years old, as of this past {birthMonth}. They live in {location}.`;
+
+console.log(sentence);
+
+var name = "Hosea";
+var age = 7;
+
+console.log(sentence);
